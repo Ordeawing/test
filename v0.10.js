0be5933056bf84194c72055de1f4fd7085043007
Ordeawing <Ordeawing@users.noreply.github.com>
2024-10-10 17:57:12 +0000

    v0.10

---
+++ v0.10.js
@@ -0,0 +1,31 @@
+(() => {
+    window.addEventListener('load', () => {
+        console.log(window.location.href);
+        let toDelete = "Toy Story";
+        if (window.location.href.includes("details?")) {
+            setTimeout(() => {
+                let parentCollection = Array.from(document.getElementsByClassName('parentNameLast'));
+                let childCollection = Array.from(document.getElementsByClassName('parentName'));
+
+                console.log(parentCollection);
+                console.log(childCollection);
+
+                parentCollection.forEach(item => {
+                    let text = item.innerText;
+                    let html = item.innerHTML;
+                    if (text == toDelete) {
+                        item.innerHTML = `${html} | <span style="color:red"> Va bientôt quitter le catalogue !</span>`;
+                    }
+                });
+
+                childCollection.forEach(item => {
+                    let text = item.innerText;
+                    let html = item.innerHTML;
+                    if (text == toDelete) {
+                        item.innerHTML = `${html} | <span style="color:red"> Va bientôt quitter le catalogue !</span>`;
+                    }
+                });
+            }, 2000);
+        }
+    });
+})();
