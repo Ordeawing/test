8557d27b2c28b7add1bc0f6465d429085c9d4593
Ordeawing <Ordeawing@users.noreply.github.com>
2024-10-10 20:30:20 +0000

    v0.12

---
+++ v0.12.js
@@ -0,0 +1,40 @@
+(() => {
+    window.addEventListener('load', async () => {
+        console.log(window.location.href);
+        if (window.location.href.includes("details?")) {
+            let url = 'https://raw.githubusercontent.com/Ordeawing/test/refs/heads/main/willGo.lst';
+            let res = await fetch(url);
+            if (res.status !== 200) return;
+            let list = (await res.text()).split('\n');
+
+            setTimeout(() => {
+                let parentCollection = Array.from(document.getElementsByClassName('parentNameLast'));
+                let childCollection = Array.from(document.getElementsByClassName('parentName'));
+
+                console.log(parentCollection);
+                console.log(childCollection);
+
+                list.some(willGo => {
+                    if (parentCollection.item(0).innerText == willGo) {
+                        parentCollection.forEach(item => {
+                            let text = item.innerText;
+                            let html = item.innerHTML;
+                            if (text == willGo) {
+                                item.innerHTML = `${html} | <span style="color:red"> Va bientôt quitter le catalogue !</span>`;
+                            }
+                        });
+        
+                        childCollection.forEach(item => {
+                            let text = item.innerText;
+                            let html = item.innerHTML;
+                            if (text == willGo) {
+                                item.innerHTML = `${html} | <span style="color:red"> Va bientôt quitter le catalogue !</span>`;
+                            }
+                        });
+                        return true;
+                    }
+                });
+            }, 1000);
+        }
+    });
+})();
